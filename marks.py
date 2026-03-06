1721b4cf773fcd3e0f2c89c8d1a7ed76947b3423
Devesh Vijay Patil <deveshpatil450@gmail.com>
2026-03-06 13:24:29 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,17 @@
+marks = 2
+
+if (marks >= 90):
+    print("A grade")
+elif (marks >= 80):
+    print("B grade")
+elif (marks >= 70):
+    print("C grade")
+elif (marks >= 50):
+    print("D grade")
+elif (marks >= 35):
+    print("E grade")
+elif (marks >= 30):
+    print("F grade")
+else:
+    print("Failed")
+
