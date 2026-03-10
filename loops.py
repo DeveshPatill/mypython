fb5f1ceedd44f01024563ca8dcea3c962985074a
Devesh Vijay Patil <deveshpatil450@gmail.com>
2026-03-10 20:55:09 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,43 @@
+# list1 = [1,2,3,4,5,6,7,8,9,10]
+# for i in list1:
+#     print(i)
+# print("-----------------------------------")
+
+# for j in range(10,101):
+#     print(j)
+
+
+# a = 4
+# while a < 10:
+#     print("asdf")
+#     a += 1
+
+
+
+# for i in range(1,11):
+#         for j in range(i):
+#             print("*", end="") 
+#         print("")
+
+
+# num = 10
+# for i in range(1,num):
+#     for j in range(i,num):
+#         print(" ",end="")
+#     for k in range(i):
+#         print("*",end="")
+#     print("")
+
+# num = 8
+# for i in range(1,num):
+#     print("",7-i)
+#     for j in range(2*i):
+#         print("*",end="")
+
+
+         
+    
+
+
+        
+
