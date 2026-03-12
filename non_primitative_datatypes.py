f7bf9c2e506e2a03f2dffee5db4f6b61a7f62043
Devesh Vijay Patil <deveshpatil450@gmail.com>
2026-03-12 14:56:09 +0530

    Add files via upload

---
+++ non_primitative_datatypes.py
@@ -0,0 +1,19 @@
+my_list = ["item1", "item2", "item3"]
+print(my_list[1])
+my_list[1] = 200
+print(my_list)
+# original_list = [1, 2, 3, 4, 5]
+reversed_list = my_list[::-1]
+print(reversed_list)
+
+
+my_tuple = ("item1", "item2", "item3")
+print(my_tuple[2])
+# my_tuple[2] = "apple"
+# print(my_tuple)
+
+my_dict = {'key1': "value1", 'key2': "value2"}
+print(my_dict["key2"])
+
+my_set = {"item1", "item2", "item3"}
+print(my_dict)
