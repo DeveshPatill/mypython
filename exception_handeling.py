3c1445488d62968cfd139fca453975e84f9447a6
Devesh Vijay Patil <deveshpatil450@gmail.com>
2026-03-18 10:35:50 +0530

    Add files via upload

---
+++ exception_handeling.py
@@ -0,0 +1,8 @@
+print("asddcdcdcdcdcdcdcfasdf")
+try:
+    print(3/0)
+except ZeroDivisionError:
+    print("its a error please try something else")
+    print("rtydcdcdcdcdcdcdcdcdcdcui")
+# finally:
+#     print("try again")
